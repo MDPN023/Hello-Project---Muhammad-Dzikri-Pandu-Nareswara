367f05a9eaf2bc149b0c7c13039c55d5f4851099
Muhammad Dzikri Pandu Nareswara <mdpnareswara@student.telkomuniversity.ac.id>
2022-03-18 11:20:40 +0700

    My FIrst

---
+++ 1.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int
+	var a bool
+	fmt.Print("Silahkan masukkan bilangan yang ingin dimasukkan : ")
+	fmt.Scan(&x)
+	if x%2 == 0 {
+		a = true
+		fmt.Print(a)
+	} else {
+		fmt.Print(a)
+	}
+}
